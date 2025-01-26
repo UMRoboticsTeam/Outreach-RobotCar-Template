f94e85961b1f9ebd06f82d018ac3336a12259f77
Kevin B <54726993+Kordevon@users.noreply.github.com>
2025-01-26 14:57:00 -0600

    Motor Control and Ultrasonic Measurement
    Added ultrasonic measurement and dc motor movement controls.

---
+++ Main.ino
@@ -0,0 +1,99 @@
+#include <Servo.h>
+
+//Motor Pin definitions. A = Right, B = Left
+#define PIN_Motor_PWMA 5 //right motor PWM
+#define PIN_Motor_PWMB 6 //left Motor PWM
+#define PIN_Motor_BIN_1 8 //Write Motor Speed
+#define PIN_Motor_AIN_1 7
+#define PIN_Motor_STBY 3
+
+//Ultrasonic PIN definitions
+#define ECHO 12
+#define TRIG 13
+
+//Servo motor pin
+#define ULTRA_Serv 10 //servo motor for ultra sensor.
+
+
+void setup() {
+  
+  
+}
+
+void loop() {
+  
+  
+}
+
+/*
+ * Uses Ultrasonic sensor to measure distance.
+ * 
+ * returns an int value representing the distance of an object infront of the sensor in CM. 
+ */
+int measureDistance()
+{
+  digitalWrite(TRIG,LOW); //set Trigger to low
+  delayMicroseconds(20); //delaying to ensure trig is low.
+  digitalWrite(TRIG, HIGH); //Set to high
+  delayMicroseconds(10); // 10 send 10-microsecond oulse.
+  digitalWrite(TRIG, LOW); //set to low.
+  delayMicroseconds(2);
+  
+  //calculating distance using speed of sound. Divide by 2 because sensor measures time of wave there and back
+  return (pulseIn(ECHO,HIGH)*0.034)/2; 
+}
+
+/*
+ * Sets Motors to drive foward
+ */
+void driveForward(int speed)
+{
+  digitalWrite(PIN_Motor_STBY, HIGH);
+  digitalWrite(PIN_Motor_AIN_1, HIGH);
+  digitalWrite(PIN_Motor_BIN_1, HIGH);
+  analogWrite(PIN_Motor_PWMA, speed);
+  analogWrite(PIN_Motor_PWMB, speed);
+}
+
+/*
+ * Sets motors to drive in reverse.
+ */
+void driveReverse(int speed){
+  digitalWrite(PIN_Motor_STBY, HIGH);
+  digitalWrite(PIN_Motor_AIN_1, LOW);
+  digitalWrite(PIN_Motor_BIN_1, LOW);
+  analogWrite(PIN_Motor_PWMA, speed);
+  analogWrite(PIN_Motor_PWMB, speed);
+}
+
+
+/*
+ * Function turns the robot car to the right. Spins right left wheels forward, right wheels backward.
+ */
+void driveRight(int speed){
+  digitalWrite(PIN_Motor_STBY, HIGH);
+  digitalWrite(PIN_Motor_AIN_1, LOW);
+  digitalWrite(PIN_Motor_BIN_1, HIGH);
+  analogWrite(PIN_Motor_PWMA, speed);
+  analogWrite(PIN_Motor_PWMB, speed);
+}
+
+/*
+ * Function turns the robot car to the right. Spins right left wheel backward, right wheels forward.
+ */
+void driveLeft(int speed){
+  digitalWrite(PIN_Motor_STBY, HIGH);
+  digitalWrite(PIN_Motor_AIN_1, HIGH);
+  digitalWrite(PIN_Motor_BIN_1, LOW);
+  analogWrite(PIN_Motor_PWMA, speed);
+  analogWrite(PIN_Motor_PWMB, speed);
+}
+
+/*
+ * Stops motors from turning
+ */
+void driveStop(){
+  digitalWrite(PIN_Motor_STBY, LOW);
+  analogWrite(PIN_Motor_PWMA, 0);
+  analogWrite(PIN_Motor_PWMB, 0);
+}
---
+++ Readme.txt
@@ -0,0 +1,27 @@
+
+Description:
+    This Project is meant to be used as a template for the Elegoo Robot Cars that are used by the UMRT Outreach Team.
+
+Purpose:
+    The main purpose of this project is for you to use this as a starting point for your own projects. 
+    Please clone the repo and then start a new GitHub repo for your own projects.
+
+
+Functions Provided:
+
+Movement:
+    - Forward
+    - Reverse
+    - turn Right
+    - turn left
+Sensor:
+    - Ultrasonic distance measurement
+
+
+ToDo:
+    Required:
+    - UtraSonic Sensor
+    - IR Sensor
+    Nice to haves:
+    - Camera
+    - Gyro
